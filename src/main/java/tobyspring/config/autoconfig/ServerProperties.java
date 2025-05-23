922d5a16b6bbe391a5f0cc9134e6bae0c090b94a
JR <sjlee@cookat.co>
2025-05-23 17:57:38 +0900

    섹션 10 완료

--- src/main/java/tobyspring/config/autoconfig/ServerProperties.java
+++ src/main/java/tobyspring/config/autoconfig/ServerProperties.java
@@ -1,5 +1,8 @@
 package tobyspring.config.autoconfig;
 
+import tobyspring.config.MyAutoConfigurationProperties;
+
+@MyAutoConfigurationProperties(prefix = "server")
 public class ServerProperties {
 
     private String contextPath;
